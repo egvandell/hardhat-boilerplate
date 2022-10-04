6ed5b5670c71e79840ed9d75558d514360ec2dcc
Greg Van Dell <gvandell@engineation.com>
2022-10-04 14:25:28 -0700

    test only

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,6 +7,7 @@ pragma solidity ^0.8.9;
 // We import this library to be able to use console.log
 import "hardhat/console.sol";
 
+//test comment
 
 // This is the main building block for smart contracts.
 contract Token {
